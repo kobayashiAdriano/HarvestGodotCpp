1a6255ee8503b88b1ed6ee0168a2683bf6395a8d
kobayashiAdriano <kobayashi.massaharo@gmail.com>
2025-06-17 00:53:28 -0300

    Atualizar register_types

--- src/register_types.cpp
+++ src/register_types.cpp
@@ -1,32 +1,24 @@
-#include "../include/register_types.hpp"
 #include "../include/example_class.hpp"
 
+#include <gdextension_interface.h>
 #include <godot_cpp/core/class_db.hpp>
+#include <godot_cpp/godot.hpp>
 
-using namespace godot;
-
-void initialize_example_module(ModuleInitializationLevel p_level) {
-    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-        return;
+void initialize_example_module(godot::ModuleInitializationLevel p_level) {
+    if (p_level == godot::MODULE_INITIALIZATION_LEVEL_SCENE) {
+        godot::ClassDB::register_class<ExampleClass>();
     }
-
-    ClassDB::register_class<ExampleClass>();
 }
 
-void uninitialize_example_module(ModuleInitializationLevel p_level) {
-    if (p_level != MODULE_INITIALIZATION_LEVEL_SCENE) {
-        return;
-    }
+void uninitialize_example_module(godot::ModuleInitializationLevel p_level) {
 }
 
 extern "C" {
-GDExtensionBool GDE_EXPORT example_library_init(const GDExtensionInterface *p_interface,
-                                               GDExtensionClassLibraryPtr p_library,
-                                               GDExtensionInitialization *r_initialization) {
-    static GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
+GDExtensionBool GDE_EXPORT example_module_init(const GDExtensionInterface *p_interface, godot::GDExtensionClassLibraryPtr p_library, godot::GDExtensionInitialization *r_initialization) {
+    godot::GDExtensionBinding::InitObject init_obj(p_interface, p_library, r_initialization);
     init_obj.register_initializer(initialize_example_module);
     init_obj.register_terminator(uninitialize_example_module);
-    init_obj.set_minimum_library_initialization_level(MODULE_INITIALIZATION_LEVEL_SCENE);
+    init_obj.set_minimum_library_initialization_level(godot::MODULE_INITIALIZATION_LEVEL_SCENE);
     return init_obj.init();
 }
 }
